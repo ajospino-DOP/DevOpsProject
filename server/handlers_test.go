d54288cdaa74c7d4d5cbb4b039e83f646d9264b9
ajospino <ajospinoa@eafit.edu.co>
2023-05-04 10:27:51 -0500

    Jenkins Test

--- server/handlers_test.go
+++ server/handlers_test.go
@@ -30,7 +30,7 @@ func Test_index(t *testing.T) {
 		})
 }
 
-//Jenkins test for update status - 3
+//Jenkins test for update status - 4
 func Test_listInfo(t *testing.T) {
 	godotenv.Load("../.env")
 	w :=  httptest.NewRecorder()
